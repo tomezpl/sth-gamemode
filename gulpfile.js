19fb11e8c7b2add5f500c047b94bcde7f7f5afd0
Tomasz Zając <tomezpl@gmail.com>
2020-11-08 17:32:28 +0000

    Adds build system with Gulp

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+﻿const gulp = require("gulp");
+
+const exec = require("child_process").exec;
+
+function build(cb) {
+    exec("node ./build.js ./client.js ./server.js");
+    cb();
+}
+
+exports.default = () => {
+    gulp.watch(["client.js", "server.js"], build);
+};
