3aac6ed316d60d0ee39ec0d9d1bdbd87bf77ed58
Tomasz Zając <tomezpl@gmail.com>
2020-11-15 01:32:33 +0000

    Updates cfx

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,7 @@
+fx_version 'cerulean'
 resource_type 'gametype' { name = 'Survive the Hunt' }
 
-client_script 'compiled/client.js'
-server_script 'compiled/server.js'
+client_scripts { 'compiled/client.js' }
+server_scripts { 'compiled/server.js' }
 
 game 'gta5'
-fx_version 'bodacious'
