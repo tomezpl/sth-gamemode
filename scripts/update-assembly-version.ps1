2527cccb3f4ed8fc1a7ec3fb527164fddd3dbf4f
Tomasz Zając <tomezpl@gmail.com>
2023-09-17 01:29:33 +0100

    ci: move release job into reusable workflow

--- scripts/update-assembly-version.ps1
+++ scripts/update-assembly-version.ps1
@@ -2,7 +2,7 @@ $TagName = $Args[0]
 
 $AssemblyInfo = cat .\Properties\AssemblyInfo.cs
 $NewVersion = $TagName.Substring(1)
-$AssemblyInfo = $AssemblyInfo -replace '^\[assembly\: AssemblyVersion\(\"\d+\.\d+\.\d+\.\d+\"\)\]$', ('[assembly: AssemblyVersion("' + $NewVersion + '.0")]')
-$AssemblyInfo = $AssemblyInfo -replace '^\[assembly\: AssemblyFileVersion\(\"\d+\.\d+\.\d+\.\d+\"\)\]$', ('[assembly: AssemblyFileVersion("' + $NewVersion + '.0")]')
+$AssemblyInfo = $AssemblyInfo -replace '^\[assembly\: AssemblyVersion\(\"\d+\.\d+\.\d+\.\d+\"\)\]$', ('[assembly: AssemblyVersion("' + $NewVersion + '")]')
+$AssemblyInfo = $AssemblyInfo -replace '^\[assembly\: AssemblyFileVersion\(\"\d+\.\d+\.\d+\.\d+\"\)\]$', ('[assembly: AssemblyFileVersion("' + $NewVersion + '")]')
 Remove-Item .\Properties\AssemblyInfo.cs
 Out-File -FilePath .\Properties\AssemblyInfo.cs -InputObject $AssemblyInfo
