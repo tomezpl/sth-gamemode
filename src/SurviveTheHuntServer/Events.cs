8430f3ab29017914b8acc0680029fb28e0fa75cf
Tomasz Zajac <tomezpl@gmail.com>
2024-02-04 16:11:42 +0000

    fix: fixed existing player blips not being replicated for players who join

--- src/SurviveTheHuntServer/Events.cs
+++ src/SurviveTheHuntServer/Events.cs
@@ -33,15 +33,6 @@ namespace SurviveTheHuntServer
         public void ClientStarted([FromSource] Player joinedPlayer)
         {
             BroadcastConfig(joinedPlayer, Config);
-            StringBuilder playerBlipInfoBuilder = new StringBuilder();
-            foreach(Player player in Players)
-            {
-                bool isHunted = GameState.Hunt.HuntedPlayer?.Handle == player.Handle;
-                playerBlipInfoBuilder.Append($"{NetworkGetNetworkIdFromEntity(player.Character.Handle)},{player.Handle},{player.Name},{(isHunted ? '1' : '0')};");
-            }
-            playerBlipInfoBuilder.Length--;
-
-            TriggerClientEvent(joinedPlayer, "sth:updatePlayerBlipBulk", playerBlipInfoBuilder.ToString());
         }
 
         [EventHandler("sth:playerSpawned")]
@@ -53,6 +44,27 @@ namespace SurviveTheHuntServer
             SetEntityDistanceCullingRadius(playerPed, float.MaxValue);
             await Delay(1000);
             TriggerClientEvent("sth:updatePlayerBlip", NetworkGetNetworkIdFromEntity(playerPed), playerId, playerName, GameState.Hunt.HuntedPlayer?.Handle == Players[playerId].Handle);
+
+
+            // Wait 5s so that player peds have been replicated to the client (hopefully).
+            if (Players[playerId].State.Get("sth:spawnedOnce") != true)
+            {
+                Players[playerId].State.Set("sth:spawnedOnce", true, false);
+
+                await Delay(5000);
+                StringBuilder playerBlipInfoBuilder = new StringBuilder();
+                foreach (Player player in Players)
+                {
+                    bool isHunted = GameState.Hunt.HuntedPlayer?.Handle == player.Handle;
+                    playerBlipInfoBuilder.Append($"{NetworkGetNetworkIdFromEntity(player.Character.Handle)},{player.Handle},{player.Name},{(isHunted ? '1' : '0')};");
+                }
+                playerBlipInfoBuilder.Length--;
+
+                Debug.WriteLine($"Sending player blip info to {playerName} because they are spawning for the first time.");
+
+                string playerBlipInfo = playerBlipInfoBuilder.ToString();
+                TriggerLatentClientEvent(Players[playerId], "sth:updatePlayerBlipBulk", (playerBlipInfo.Length * sizeof(char)) / 2, playerBlipInfo);
+            }
 
             /*Debug.WriteLine($"Cleaning clothes for {Players[playerId].Name}");
             TriggerClientEvent("sth:cleanClothesForPlayer", new { PlayerId = playerId });*/
