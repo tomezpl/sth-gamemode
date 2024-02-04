d672d17faa91afeae9d80d95297bbf14029caf17
Tomasz Zajac <tomezpl@gmail.com>
2024-02-04 19:22:26 +0000

    chore: reduced player blip restore timer

--- src/SurviveTheHuntServer/Events.cs
+++ src/SurviveTheHuntServer/Events.cs
@@ -51,7 +51,7 @@ namespace SurviveTheHuntServer
             {
                 Players[playerId].State.Set("sth:spawnedOnce", true, false);
 
-                await Delay(5000);
+                await Delay(3000);
                 StringBuilder playerBlipInfoBuilder = new StringBuilder();
                 foreach (Player player in Players)
                 {
