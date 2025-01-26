56b428e7fd0fc18ce97d5f98f346107d5642a070
Tomasz Zajac <tomezpl@gmail.com>
2025-01-26 21:34:13 +0000

    ci: fixed ilmerge path

--- uimanifest.lua
+++ uimanifest.lua
@@ -1,6 +1,8 @@
 fx_version 'cerulean'
 
-client_scripts { 'SurviveTheHuntClient.UI.net.dll' }
+client_scripts {'SurviveTheHuntClient.UI.net.dll' }
 shared_scripts { 'SurviveTheHuntShared.net.dll' }
+
+file 'LemonUI.FiveM.dll'
 
 game 'gta5'
