f0b6fd36438f1226a2c2b8467033746d20e03465
Tomasz Zajac <tomezpl@gmail.com>
2025-02-23 17:45:26 +0000

    feat(client/MusicPlayer): added switching tracks

--- src/SurviveTheHuntClient/Helpers/MusicPlayer.cs
+++ src/SurviveTheHuntClient/Helpers/MusicPlayer.cs
@@ -24,11 +24,20 @@ namespace SurviveTheHuntClient.Helpers
                 "CAR1_MISSION_START",
                 "CAR1_CHASE_START"
             };
+
+            public readonly string[] Stealth =
+            {
+                "ARM3_START",
+                "ARM3_MIC"
+            };
         }
 
         internal static AllEventsType AllEvents = new AllEventsType();
 
         internal static readonly string[] AllEventsNames = BuildAllEventsCache();
+
+        private static bool isPlaying = false;
+        internal static bool IsPlaying { get => isPlaying; }
 
         /// <summary>
         /// Gets all music event names from <see cref="AllEvents"/> and creates a flat string array of music event names.
@@ -49,13 +58,70 @@ namespace SurviveTheHuntClient.Helpers
             return eventNames.ToArray();
         }
 
-        internal static void ClearMusic()
+        /// <summary>
+        /// Cancels all music events and optionally stops their currently active playback.
+        /// </summary>
+        /// <param name="stop">Should all score stop playing?</param>
+        internal static void ClearMusic(bool stop = true)
         {
             foreach(string eventName in AllEventsNames)
             {
                 CancelMusicEvent(eventName);
             }
+
+            if (stop)
+            {
+                StopMusicRightNow();
+                isPlaying = false;
+            }
+        }
+
+        internal static void Init()
+        {
+            foreach(string eventName in AllEventsNames)
+            {
+                PrepareMusicEvent(eventName);
+            }
+
             PrepareMusicEvent("GTA_ONLINE_STOP_SCORE");
+        }
+
+        /// <summary>
+        /// Activates selected music
+        /// </summary>
+        /// <param name="events">Music event(s) to activate - this should be a field from <see cref="AllEvents"/></param>
+        internal static void PlayMusic(string[] events)
+        {
+            if(isPlaying)
+            {
+                ClearMusic(false);
+            }
+
+            bool success = true;
+            foreach(string evName in events)
+            {
+                success = TriggerMusicEvent(evName);
+                if(!success)
+                {
+                    break;
+                }
+            }
+
+            if(!success)
+            {
+                foreach(string evName in events)
+                {
+                    // Cancel the requested events as soon as any of them failed to play
+                    CancelMusicEvent(evName);
+                }
+                StopMusicRightNow();
+            }
+
+            isPlaying = success;
+        }
+
+        private static void StopMusicRightNow()
+        {
             TriggerMusicEvent("GTA_ONLINE_STOP_SCORE");
         }
     }
