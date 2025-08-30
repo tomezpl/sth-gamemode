97a034c72dbe4da958f9ff6ca317a3f6f992647d
Tomasz Zajac <tomezpl@gmail.com>
2025-08-30 14:47:50 +0100

    fix(client): fixed passenger seat weaponised vehicle exploit

---
+++ src/SurviveTheHuntClient/Helpers/VehicleWeaponsTracker.cs
@@ -0,0 +1,26 @@
+﻿using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+
+namespace SurviveTheHuntClient.Helpers
+{
+    /// <summary>
+    /// Helper for disabling weaponised vehicles' guns
+    /// </summary>
+    internal static class VehicleWeaponsTracker
+    {
+        internal static void Tick()
+        {
+            Vehicle veh = Game.PlayerPed.CurrentVehicle;
+            if(veh?.Exists() == true && DoesVehicleHaveWeapons(veh.Handle))
+            {
+                uint weapon = uint.MaxValue;
+
+                // If the player ped has a vehicle weapon equipped, disable it
+                if(GetCurrentPedVehicleWeapon(Game.PlayerPed.Handle, ref weapon))
+                {
+                    DisableVehicleWeapon(true, weapon, veh.Handle, Game.PlayerPed.Handle);
+                }
+            }
+        }
+    }
+}
