c47d3b0edc0341b961cd7cffda1245cc7f74f126
Tomasz Zajac <tomezpl@gmail.com>
2026-04-26 15:13:47 +0100

    fix(client-plugins/cupid): tweaked robbery locations

--- src/SurviveTheHuntClient.Plugins.Cupid/Constants/Locations.cs
+++ src/SurviveTheHuntClient.Plugins.Cupid/Constants/Locations.cs
@@ -8,11 +8,12 @@ namespace SurviveTheHuntClient.Plugins.Cupid
         {
             internal static class RobberyJob
             {
-                internal static Vector3 FleecaLegion => new Vector3(152.26f, -1034.67f, 30.15f);
+                internal static Vector3 FleecaLegion => new Vector3(152.26f, -1034.67f, 29.15f);
 
-                internal static Vector3 FleecaRockford => new Vector3(-1216.75f, -322.44f, 37.7f);
+                internal static Vector3 FleecaRockford => new Vector3(-1216.75f, -322.44f, 36.3f);
+                internal static Vector3 FleecaRockfordObjective => new Vector3(-1210.809f, -336.4966f, 36.38103f);
 
-                internal static Vector3 FleecaChumash => new Vector3(-2967.11f, 483f, 16.28f);
+                internal static Vector3 FleecaChumash => new Vector3(-2967.11f, 483f, 14.08f);
             }
         }
     }
